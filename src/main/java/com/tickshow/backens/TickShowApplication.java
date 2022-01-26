5c6976b27d4021b6567f0a4c187f67aa5fd57ef3
Pragashraj <rajpragashraj30@gmail.com>
2022-01-26 16:52:45 +0530

    Project Initiated

---
+++ src/main/java/com/tickshow/backens/TickShowApplication.java
@@ -0,0 +1,12 @@
+package com.tickshow.backens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickShowApplication.class, args);
+    }
+}
